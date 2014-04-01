c9c30096e4c3fc6fc13db1ab837b656d4e2fb1e5
Paul Likoudis <likoudis@hotmail.com>
2014-04-01 08:46:51 -0500

    Introduced Drawer

--- Nea Smirni/scripts/app.js
+++ Nea Smirni/scripts/app.js
@@ -1,15 +1,23 @@
 (function (global) {
     var app = global.app = global.app || {};
-
+	
     document.addEventListener('deviceready', function () {
         navigator.splashscreen.hide();
     }, false);
 
-    app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout"});
+   app.application = new kendo.mobile.Application(document.body, {});
 
-	app.closeLoginModalView = function () {
+	app.isLoggedIn = false;
+
+	app.closeLoginModalView = function (e) {
         $("#loginModalView").kendoMobileModalView("close");
-    }
-
-
+		app.isLoggedIn = !app.isLoggedIn;
+        $("#openHomePage").data("kendoMobileButton").enable(app.isLoggedIn);
+    };
+	
+	app.foo = {
+		init: function () {
+		    $("#my-drawer").data("kendoMobileDrawer").show();
+        }
+    };
 })(window);
