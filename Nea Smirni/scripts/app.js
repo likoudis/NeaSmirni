cf11133f678b1cf938b6bc9da927cfe11d9d330a
Paul Likoudis <likoudis@hotmail.com>
2014-04-08 02:03:37 -0500

    Added login modalView

--- Nea Smirni/scripts/app.js
+++ Nea Smirni/scripts/app.js
@@ -5,19 +5,15 @@
         navigator.splashscreen.hide();
     }, false);
 
-   app.application = new kendo.mobile.Application(document.body, {});
+	app.application = new kendo.mobile.Application(document.body, {});
 
 	app.isLoggedIn = false;
 
 	app.closeLoginModalView = function (e) {
         $("#loginModalView").kendoMobileModalView("close");
-		app.isLoggedIn = !app.isLoggedIn;
-        $("#openHomePage").data("kendoMobileButton").enable(app.isLoggedIn);
-    };
-	
-	app.foo = {
-		init: function () {
-		    $("#my-drawer").data("kendoMobileDrawer").show();
+		if (e.sender.element.text().trim() !== "Cancel") {
+			app.isLoggedIn = !app.isLoggedIn;
+			$("#openHomePage").data("kendoMobileButton").enable(app.isLoggedIn);
         }
     };
 })(window);
