bd5278583182d8495f1e353064d6701c0f902375
Paul Likoudis <likoudis@hotmail.com>
2014-04-22 09:05:25 -0500

    Elementary gallery

--- Nea Smirni/scripts/app.js
+++ Nea Smirni/scripts/app.js
@@ -8,6 +8,8 @@
 	app.application = new kendo.mobile.Application(document.body, {});
 
 	app.isLoggedIn = false;
+	
+	app.imageCount = 0;
 
 	app.closeLoginModalView = function (e) {
         $("#loginModalView").kendoMobileModalView("close");
@@ -18,60 +20,53 @@
     };
 	
 	app.onSnapShow = function (e) {
-			sessionStorage.removeItem('imagepath');
-			// Take picture using device camera and retrieve image as base64-encoded string
-			navigator.camera.getPicture(
-				onPhotoDataSuccess, 
-				onFail, { 
-					quality: 50, 
-					destinationType: Camera.DestinationType.FILE_URI
-				}
-			);
-		}
-		
-		function onPhotoDataSuccess(imageURI) { 
-			// Uncomment to view the base64 encoded image data
-			// console.log(imageData);
-		
-			window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, resOnError); 
-		}
-
+		// Take picture using device camera and retrieve image file
+		navigator.camera.getPicture(
+			onPhotoDataSuccess, 
+			onFail, 
+			{   quality: 50, 
+				destinationType: Camera.DestinationType.FILE_URI
+			}
+		);
+	}
+	
+	function onPhotoDataSuccess(imageURI) { 
+		window.resolveLocalFileSystemURI(imageURI, resolveOnSuccess, resOnError); 
+	}
 		function onFail(message) {
-			alert('Failed because: ' + message);
-		}
-
+		alert('Failed because: ' + message);
+	}
 		//Callback function when the file system uri has been resolved
-		function resolveOnSuccess(entry){ 
-			//new file name
-			var newFileName = new Date();
-			newFileName = newFileName.getTime();
-			newFileName = newFileName + ".jpg";
-			var NSAppFolder = "MyInspections";
-		
-			window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {
-			//The folder is created if doesn't exist
-				fileSys.root.getDirectory( NSAppFolder,
-					{create:true, exclusive: false},
-					function(directory) {
-						entry.moveTo(directory, newFileName,  successMove, resOnError);
-					},
-					resOnError
-				);
-			},
-			resOnError);
-		}
-		
-		//Callback function when the file has been moved successfully - inserting the complete path
-		function successMove(entry) {
-			//Store imagepath in session for future use
-			// like to store it in database
-			sessionStorage.setItem('imagepath', entry.fullPath);
-			document.getElementById("snap-fname").innerHTML=entry.fullPath;
-			document.getElementById("snap-thumb").src=entry.fullPath;
-			//document.getElementById("snap-thumb").src="styles/images/My_municipality.png";
-		}
-		
-		function resOnError(error) {
-			alert(error.code);
-		}
+	function resolveOnSuccess(entry){ 
+		//new file name
+		var fname = app.imageCount;
+		fname = fname < 100 ? fname < 10 ? "00"+ fname : "0" + fname : fname ;
+		fname = "000000000_" + fname + ".jpg";
+		var NSAppFolder = "MyInspections";
+	
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSys) {
+		//The folder is created if doesn't exist
+			fileSys.root.getDirectory( NSAppFolder,
+				{create:true, exclusive: false},
+				function(directory) {
+					entry.moveTo(directory, fname,  successMove, resOnError);
+				},
+				resOnError
+			);
+		},
+		resOnError);
+	}
+	
+	//Callback function when the file has been moved successfully - inserting the complete path
+	function successMove(entry) {
+		app.imageCount = app.imageCount < 999 ? app.imageCount + 1 : 0;
+		document.getElementById("snap-fname").innerHTML=entry.fullPath;
+		document.getElementById("snap-thumb").src=entry.fullPath;
+	//document.getElementById("snap-thumb").src="styles/images/My_municipality.png";
+	}
+	
+	function resOnError(error) {
+		alert(error.code);
+	}
+
 })(window);
